5074032d323e09ab25dd99e985934fac37ecd2ca
Zlepper <hansen13579@gmail.com>
2017-10-10 17:10:49 +0200

    Unit testing tests

--- src/test/kotlin/dk/group11/auditsystem/AuditSystemApplicationTests.kt
+++ src/test/kotlin/dk/group11/auditsystem/AuditSystemApplicationTests.kt
@@ -2,11 +2,13 @@ package dk.group11.auditsystem
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@DataJpaTest
 class AuditSystemApplicationTests {
 
     @Test
